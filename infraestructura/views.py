3b4f421f4e02812b31bcfe40168bda32e3a2ebb1
Darwin Munoz <ddmunoz@>
2026-09-04 21:13:50 -0500

    Creación de Formularios para crear, editar, eliminar y se creo botones para el control de estos mismos

--- infraestructura/views.py
+++ infraestructura/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
 from .models import NodoServidor
+from .forms import NodoServidorForm
+
+def crear_servidor(request):
+    if request.method == 'POST':
+        form = NodoServidorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home_servidores')
+    else:
+        form = NodoServidorForm()
+    return render(request, 'infraestructura/crear_servidor.html', {'form': form})
+
+def editar_servidor(request, pk):
+    nodo = get_object_or_404(NodoServidor, pk=pk)
+    if request.method == 'POST':
+        form = NodoServidorForm(request.POST, instance=nodo)
+        if form.is_valid():
+            form.save()
+            return redirect('detalle_servidor', pk=nodo.pk)
+    else:
+        form = NodoServidorForm(instance=nodo)
+    return render(request, 'infraestructura/editar_servidor.html', {'form': form, 'nodo': nodo})
+
+def eliminar_servidor(request, pk):
+    nodo = get_object_or_404(NodoServidor, pk=pk)
+    if request.method == 'POST':
+        nodo.delete()
+        return redirect('home_servidores')
+    return render(request, 'infraestructura/eliminar_servidor.html', {'nodo': nodo})
+
+def detalle_servidor(request, pk):
+	nodo = get_object_or_404(NodoServidor, pk=pk)
+	return render(request, "infraestructura/detalles.html",context={'nodo':nodo})
 
 def lista_servidores(request):
 	servidores = NodoServidor.objects.all()
